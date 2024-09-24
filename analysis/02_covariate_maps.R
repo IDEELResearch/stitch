43141c64bbcae4fa91ebf127bb23e63d6c620310
shaziaruybal <shaziaruybal@gmail.com>
2024-09-24 13:54:44 +0100

    update 02 covar map script to use plotting fxn

--- analysis/02_covariate_maps.R
+++ analysis/02_covariate_maps.R
@@ -17,21 +17,6 @@ map_data <- get_map_covar_data(covars)
 
 # Plot all covariates over time facets ------------------------------------
 
-covars_to_plot <- covars_df %>% select(-c(iso3c:year)) %>% names()
+covars_to_plot <- map_data %>% st_drop_geometry() %>% select(-c(iso3c:year)) %>% names()
 
-for (c in covars_to_plot) {
-  map_data_filtered <- map_data %>% filter(!is.na(.data[[c]]))
-
-  p <- map_data_filtered %>%
-    ggplot() +
-    geom_sf(aes(fill = !!sym(c)), color = NA) +
-    geom_sf(data = sf_adm0, color = "black", fill = NA) +
-    scale_fill_viridis_c(option = "viridis", labels = scales::percent_format(accuracy = 1)) +
-    coord_sf() +
-    facet_wrap(~year) +
-    theme_void(base_size = 14) +
-    theme(plot.background = element_rect(fill = "white", color = "white"))
-
-  filename <- str_glue("analysis/plots/map_{c}.png")
-  ggsave(filename, p, width = 8, height = 6, dpi = 300)
-}
+plot_spatiotemporal_map(map_data, sf_adm0, covars_to_plot)
