a8a009e26f5b1bb1a5b5c8684b930375d009f00e
shaziaruybal <shaziaruybal@gmail.com>
2024-09-24 14:18:21 +0100

    adding append_lat_lon() helper function

---
+++ R/append_lat_lon.R
@@ -0,0 +1,32 @@
+#' Get 'centroid' coordinates in lat and lon
+#'
+#' This function extracts the centroid coordinates from the geometries of a shape file object and appends them as columns
+#'
+#' @param shp The sf object
+#' @return A shape file with lat lon 'centroid' coordinates appended as columns before the geometries
+#' @export
+#'
+#' @examples
+#' shp_adm0 <- readRDS("analysis/data_derived/sf_admin0_africa.rds")
+#' shp_adm0 <- shp_adm0 %>% append_lat_lon()
+
+append_lat_lon <- function(shp){
+
+  # switching off spherical geometry
+  sf_use_s2(F)
+
+  shp <- shp %>%
+    # get geometry col
+    st_geometry() %>%
+    # get centroid of polygon
+    st_centroid() %>%
+    # convert to lat/lon coords
+    st_coordinates() %>%
+    # bind with map_data
+    cbind(shp) %>%
+    rename("lon" = "X",
+           "lat" = "Y") %>%
+    relocate(c(lat:lon), .before = geometry)
+
+  return(shp)
+}
