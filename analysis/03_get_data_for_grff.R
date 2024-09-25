a2fd37ec5ea505da40c652211453d57221991c5d
shaziaruybal <shaziaruybal@gmail.com>
2024-09-25 10:35:09 +0100

    script to get input data for grff (can go back and make an actual function within {stitch}, but for now just fxn in a script) and example input data .rds with covars

--- analysis/03_get_data_for_grff.R
+++ analysis/03_get_data_for_grff.R
@@ -4,7 +4,7 @@ library(sf)
 
 # find lat lon from stave -> merge with shp -> convert to df (remove geometries)
 
-covars <- readRDS("analysis/data_raw/final_covariates.rds")
+covars <- readRDS(url("https://github.com/IDEELResearch/scrape/raw/refs/heads/main/analysis/data-derived/final_covariates.rds"))
 stave <- readRDS(url("https://github.com/IDEELResearch/stitch/raw/refs/heads/main/analysis/data_derived/mock_stave_prevalence.rds"))
 
 map_data <- get_map_covar_data(covars)
@@ -17,6 +17,7 @@ stave %>% st_as_sf(coords = c("lon", "lat"), crs = 4326) %>%  # EPSG:4326 for WG
 get_grff_input <- function(stave_obj, covar_obj){
 
   covar_processed <- get_map_covar_data(covar_obj)
+  covar_names <- covar_processed %>% select(-c(iso3c:year)) %>% st_drop_geometry() %>% names()
 
   sf_use_s2(FALSE)
 
@@ -24,7 +25,7 @@ get_grff_input <- function(stave_obj, covar_obj){
     st_as_sf(coords = c("lon", "lat"), crs = 4326, remove = F) %>% # EPSG:4326 for WGS84 same as MAP shp files
     st_join(covar_processed, left = TRUE) %>%
     as.data.frame() %>%
-    select(lat, lon, collection_day, numerator, denominator, year, pfpr210:ASPY) %>%
+    select(lat, lon, collection_day, numerator, denominator, year, all_of(covar_names)) %>%
     mutate(collection_year = year(as.Date(collection_day))) %>%
     filter(collection_year == year) %>%
     relocate(collection_year, .after = collection_day) %>%
@@ -33,3 +34,5 @@ get_grff_input <- function(stave_obj, covar_obj){
 }
 
 input_df <- get_grff_input(stave, covars)
+
+saveRDS(input_df, "analysis/data_derived/input_for_grff.rds")
