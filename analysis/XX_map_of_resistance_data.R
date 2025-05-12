d203ed2ba580e28fcb8babe4adf30719263cbc35
nnwyoung <neeva_young@brown.edu>
2025-05-12 15:30:09 -0400

    loop for getting stave prev

--- analysis/XX_map_of_resistance_data.R
+++ analysis/XX_map_of_resistance_data.R
@@ -3,82 +3,27 @@ library(ggplot2)  # for plotting
 library(dplyr)    # for data manipulation
 library(tidyr)    # for mild clean of stave data
 
-
-#neeva learns how to commit with git 
-
-# Expanded dataframe with 15 African countries and varying admin 1 regions
-# PRACTICE DATA
-#####
-# prevalence_data <- data.frame(
-#   country = c("Uganda", "Uganda", "Uganda",
-#               "Kenya", "Kenya", "Kenya",
-#               "Tanzania", "Tanzania", "Tanzania",
-#               "Nigeria", "Nigeria", "Nigeria", "Nigeria",
-#               "Ghana", "Ghana",
-#               "South Africa", "South Africa", "South Africa",
-#               "Zambia", "Zambia",
-#               "Zimbabwe", "Zimbabwe", "Zimbabwe",
-#               "Malawi",
-#               "Ethiopia", "Ethiopia", "Ethiopia", "Ethiopia",
-#               "Senegal", "Senegal",
-#               "Côte d'Ivoire", "Côte d'Ivoire", "Côte d'Ivoire", "Côte d'Ivoire",
-#               "Angola", "Angola", "Angola", "Angola", "Angola",
-#               "Rwanda", "Rwanda", "Rwanda",
-#               "Burkina Faso", "Burkina Faso", "Burkina Faso", "Burkina Faso",
-#               "Togo", "Togo",
-#               "Cameroon", "Cameroon", "Cameroon", "Cameroon", "Cameroon",
-#               "Mali", "Mali", "Mali",
-#               "Gambia",
-#               "Somalia", "Somalia", "Somalia",
-#               "Namibia", "Namibia", "Namibia", "Namibia"),
-#   name_1 = c("Central", "Eastern", "Northern",  # Uganda
-#              "Nairobi", "Mombasa", "Kisumu",  # Kenya
-#              "Dodoma", "Arusha", "Mwanza",  # Tanzania
-#              "Lagos", "Kano", "Abuja", "Port Harcourt",  # Nigeria
-#              "Greater Accra", "Ashanti",  # Ghana
-#              "Gauteng", "Western Cape", "KwaZulu-Natal",  # South Africa
-#              "Lusaka", "Copperbelt",  # Zambia
-#              "Harare", "Bulawayo", "Manicaland",  # Zimbabwe
-#              "Blantyre",  # Malawi
-#              "Addis Ababa", "Oromia", "Amhara", "Tigray",  # Ethiopia
-#              "Dakar", "Thies",  # Senegal
-#              "Abidjan", "Bouaké", "Yamoussoukro", "Korhogo",  # Côte d'Ivoire
-#              "Luanda", "Huambo", "Benguela", "Malanje", "Kwanza Sul",  # Angola
-#              "Kigali", "Western", "Northern",  # Rwanda
-#              "Centre", "Boucle du Mouhoun", "Hauts-Bassins", "Sud-Ouest",  # Burkina Faso
-#              "Maritime", "Plateaux",  # Togo
-#              "Littoral", "Ouest", "Nord-Ouest", "Sud-Ouest", "Adamaoua",  # Cameroon
-#              "Bamako", "Kayes", "Sikasso",  # Mali
-#              "Banjul",  # Gambia
-#              "Mogadishu", "South West", "Galmudug",  # Somalia
-#              "Khomas", "Erongo", "Otjozondjupa", "Hardap"),  # Namibia,  # Namibia
-#   year = sample(2018:2022, 64, replace = TRUE),  # Random years between 2018 and 2022
-#   sample_size = sample(0:180, 64, replace = TRUE),
-#   prevalence = sample(seq(0,1,0.001), 64, replace = TRUE)
-# )
-#####
-
 # Read in Africa shape file generated in 01_generative_base_maps.R
 rds_file_admin0 = "analysis/data_derived/sf_admin0_africa.rds"
 rds_file_admin1 = "analysis/data_derived/sf_admin1_africa.rds"
 africa_shp_admin0 <- readRDS(file = rds_file_admin0)
 africa_shp_admin1 <- readRDS(file = rds_file_admin1)
 
-#Pull in Stave Data 
-stave = readRDS(url("https://github.com/IDEELResearch/scrub/blob/main/analysis/data-out/stave_data.rds"))
+#Pull in Stave Data from cleaned & selected prevalence script
+all_prev_data <- read.csv("analysis/data/validated_and_candidate_get_prevalence.csv", header = TRUE)
 
 # Clean Stave Data
-library(countrycode)  # for coordinating STAVE country with iso3c code 
+library(countrycode)  # for coordinating STAVE country with iso3c code
 
 # Get surveys from STAVE & identify countries for each lat lon
 survey <- stave$get_surveys()
 # Collapse entries to unique coords
 unique_coords <- survey %>%
-  select(lat, lon, site_name, country_name, study_key) %>%
+  select(latitude, longitude, site_name, country_name, study_key) %>%
   distinct() %>%
   mutate(
-    lat = as.numeric(lat),
-    lon = as.numeric(lon)
+    lat = as.numeric(latitude),
+    lon = as.numeric(longitude)
   ) %>%
   drop_na(lat, lon) %>%
   rename(study_ID = study_key) %>%
@@ -86,22 +31,24 @@ unique_coords <- survey %>%
 unique_coords_sf <- st_as_sf(unique_coords, coords = c("lon", "lat"), crs = 4326)
 # Align lat lon with admin shapefiles
 coords_with_admin0 <- st_join(unique_coords_sf, africa_shp_admin0, join = st_within) %>%
-  select(entry_iso3c, study_ID, iso) %>%  # Retain entry_iso3c and study_ID
+  select(entry_iso3c, study_ID)
+
+%>%  # Retain entry_iso3c and study_ID
   rename(admin0_iso3c = iso)
 coords_with_admin1 <- st_join(unique_coords_sf, africa_shp_admin1, join = st_within) %>%
   mutate(
-    lon = st_coordinates(.)[, 1],  # Extract longitude
-    lat = st_coordinates(.)[, 2]   # Extract latitude
+    longitude = st_coordinates(.)[, 1],  # Extract longitude
+    latitude = st_coordinates(.)[, 2]   # Extract latitude
   ) %>%
-  select(lon, lat, site_name, name_1) %>%
+  select(longitude, latitude, site_name, name_1) %>%
   rename(admin1_name = name_1)
-# Combine admin0 and admin1 
+# Combine admin0 and admin1
 coords_with_admin <- coords_with_admin1 %>%
   st_join(coords_with_admin0)
 # Identify any general mismatches mismatches, remove any non-African STAVE data (shape)
 mismatch_rows <- coords_with_admin %>%
   filter(entry_iso3c != admin0_iso3c | !(entry_iso3c %in% admin0_iso3c)) %>%  # Compare entry_iso3c with admin0_iso3c
-  select(lon, lat, site_name, admin1_name, entry_iso3c, admin0_iso3c, study_ID)
+  select(longitude, latitude, site_name, admin1_name, entry_iso3c, admin0_iso3c, study_ID)
 # Drop studies that are in mismatches
 mismatch_studies <- unique(mismatch_rows$study_ID)
 stave_clean = stave$clone()
@@ -181,15 +128,15 @@ for (selected_mutation in validated_mutations){
   #TO DO: potentially filter by year depending on mutation? mdr1 data extends to 96
   prevalence_data <- stave_clean$get_prevalence(selected_mutation) %>% drop_na() %>%
     mutate(year = substring(collection_day,0,4))
-  
+
   #associate prevalence data lat lons with clean admin_1 names
   prevalence_data <- left_join(prevalence_data, clean_admin1) %>% drop_na()
-  
+
   # Add prevalence data to admin 1 shape file
   africa_admin1_longlat_prev <- africa_admin1_longlat %>%
     left_join(prevalence_data, by = "name_1") %>%
     filter(!is.na(denominator))
-  
+
   # Plot the Africa map coloring districts by prevalence_size
   africa_map_sample_fill <- ggplot() +
     facet_wrap(~year) +
@@ -200,12 +147,12 @@ for (selected_mutation in validated_mutations){
     scale_fill_viridis_c() +
     theme(legend.position = "bottom",
           plot.background = element_rect(fill = "white", color="white"))
-  
+
   ggsave(
     filename=paste0("analysis/plots/africa_map_", gsub(":","_", selected_mutation), "prev_facet.png"),
     plot = africa_map_sample_fill,
     width = 12, height = 10, units = "in", dpi = 300)
-  
+
   # Plot the Africa map coloring districts by sample size
   africa_map_sample_fill <- ggplot() +
     facet_wrap(~year) +
@@ -216,12 +163,111 @@ for (selected_mutation in validated_mutations){
     scale_fill_viridis_c() +
     theme(legend.position = "bottom",
           plot.background = element_rect(fill = "white", color="white"))
-  
+
   ggsave(
     filename=paste0("analysis/plots/africa_map_", gsub(":","_", selected_mutation), "facet.png"),
     plot = africa_map_sample_fill,
     width = 12, height = 10, units = "in", dpi = 300)
-  
-  
+
+
   print(paste("Save plot for:", selected_mutation))
 }
+
+#prep stave objects by aligning coordinates and names
+#where survey_clean is the get_survey output of a clean STAVE object
+coords_clean <- survey_clean %>%
+  select(latitude, longitude, site_name, country_name, study_key) %>%
+  distinct() %>%
+  mutate(
+    latitude = as.numeric(latitude),
+    longitude = as.numeric(longitude)
+  ) %>%
+  drop_na(latitude, longitude) %>%
+  rename(study_ID = study_key) %>%
+  mutate(entry_iso3c = countrycode(country_name, origin = "country.name", destination = "iso3c"))
+coords_clean_sf <- st_as_sf(coords_clean, coords = c("longitude", "latitude"), crs = 4326)
+clean_admin1 <- st_join(coords_clean_sf, africa_shp_admin1, join = st_within) %>%
+  mutate(
+    longitude = st_coordinates(.)[, 1],  # Extract longitude
+    latitude = st_coordinates(.)[, 2]   # Extract latitude
+  ) %>%
+  select(study_ID, longitude, latitude, site_name, name_1)
+
+# Calculate the centroids for each MULTIPOLYGON
+sf_use_s2(FALSE)
+africa_admin1_longlat <- africa_shp_admin1 %>%
+  st_geometry() %>% #obtain geometry
+  st_centroid() %>% #obtain centroid of geometry
+  st_coordinates() %>% #obtain coordinates of the centroids (long, lat)
+  cbind(africa_shp_admin1) %>%
+  rename("lon" = "X",
+         "lat" = "Y")
+
+# Add prevalence data to admin 1 shape file
+africa_admin1_longlat_prev <- africa_admin1_longlat %>%
+  left_join(all_prev_data, by = "name_1") %>%
+  filter(!is.na(sample_size))
+
+# Plot the Africa map coloring districts by sample_size
+africa_map_sample_fill <- ggplot() +
+  facet_wrap(~year) +
+  geom_sf(data = africa_admin1_longlat_prev, aes(fill = sample_size), color = "darkgrey", lwd = 0.05) +
+  geom_sf(data = africa_shp_admin0, fill = NA, color = "black", show.legend = FALSE, lwd = 0.05) +
+  theme_void(base_size = 14) +
+  labs(fill = "Sample Size (N)") +
+  scale_fill_viridis_c() +
+  theme(legend.position = "bottom",
+        plot.background = element_rect(fill = "white", color="white"))
+
+ggsave(filename="analysis/plots/africa_map_sample_fill.png", africa_map_sample_fill)
+
+
+selected_mutation = "k13:561:H"
+
+for (selected_mutation in all_who_mutations){
+  #filter all_prev data to selected mutation
+  prevalence_data <- all_prev_data %>% filter(mutation == selected_mutation)
+
+  #associate prevalence data lat lons with clean admin_1 names
+  prevalence_data <- left_join(prevalence_data, clean_admin1) %>% drop_na()
+
+  # Add prevalence data to admin 1 shape file
+  africa_admin1_longlat_prev <- africa_admin1_longlat %>%
+    left_join(prevalence_data, by = "name_1") %>%
+    filter(!is.na(denominator))
+
+  # Plot the Africa map coloring districts by prevalence_size
+  africa_map_sample_fill <- ggplot() +
+    facet_wrap(~year) +
+    geom_sf(data = africa_admin1_longlat_prev, aes(fill = prevalence), color = "darkgrey", lwd = 0.05) +
+    geom_sf(data = africa_shp_admin0, fill = NA, color = "black", show.legend = FALSE, lwd = 0.05) +
+    theme_void(base_size = 14) +
+    labs(fill = "Prevalence (N)") +
+    scale_fill_viridis_c() +
+    theme(legend.position = "bottom",
+          plot.background = element_rect(fill = "white", color="white"))
+
+  ggsave(
+    filename=paste0("analysis/plots/africa_map_", gsub(":","_", selected_mutation), "prev_facet.png"),
+    plot = africa_map_sample_fill,
+    width = 12, height = 10, units = "in", dpi = 300)
+
+  # Plot the Africa map coloring districts by sample size
+  africa_map_sample_fill <- ggplot() +
+    facet_wrap(~year) +
+    geom_sf(data = africa_admin1_longlat_prev, aes(fill = denominator), color = "darkgrey", lwd = 0.05) +
+    geom_sf(data = africa_shp_admin0, fill = NA, color = "black", show.legend = FALSE, lwd = 0.05) +
+    theme_void(base_size = 14) +
+    labs(fill = "Sample Size (N)") +
+    scale_fill_viridis_c() +
+    theme(legend.position = "bottom",
+          plot.background = element_rect(fill = "white", color="white"))
+
+  ggsave(
+    filename=paste0("analysis/plots/africa_map_", gsub(":","_", selected_mutation), "facet.png"),
+    plot = africa_map_sample_fill,
+    width = 12, height = 10, units = "in", dpi = 300)
+
+  print(paste("Save plot for:", selected_mutation))
+}
+
---
+++ analysis/YY_prep_stave_object.R
@@ -0,0 +1,176 @@
+library(sf)       # for shapefile handling
+library(ggplot2)  # for plotting
+library(dplyr)    # for data manipulation
+library(tidyr)    # for mild clean of stave data
+library(here)
+library(devtools)
+
+devtools::check()
+#devtools::document()
+#devtools::load_all()
+
+# Read in Africa shape file generated in 01_generative_base_maps.R
+rds_file_admin0 = "analysis/data_derived/sf_admin0_africa.rds"
+rds_file_admin1 = "analysis/data_derived/sf_admin1_africa.rds"
+africa_shp_admin0 <- readRDS(file = rds_file_admin0)
+africa_shp_admin1 <- readRDS(file = rds_file_admin1)
+
+#Pull in Stave Data
+#stave = readRDS(url("https://github.com/IDEELResearch/scrub/tree/931d07255aadc1e41005ed45c8345d0ac966b272/analysis/data-out"))
+stave <- readRDS("/home/nwernsma/Documents/scrub/analysis/data-out/stave_data.rds")
+
+# Clean Stave Data
+library(countrycode)  # for coordinating STAVE country with iso3c code
+
+# Get surveys from STAVE & identify countries for each lat lon
+survey <- stave$get_surveys()
+
+# Collapse entries to unique coords
+sf_use_s2(FALSE)
+unique_coords <- survey %>%
+  select(latitude, longitude, site_name, country_name, study_key) %>%
+  distinct() %>%
+  mutate(
+    lat = as.numeric(latitude),
+    lon = as.numeric(longitude)
+  ) %>%
+  drop_na(lat, lon) %>%
+  rename(study_ID = study_key) %>%
+  mutate(entry_iso3c = countrycode(country_name, origin = "country.name", destination = "iso3c"))
+unique_coords_sf <- st_as_sf(unique_coords, coords = c("lon", "lat"), crs = 4326)
+
+# Align lat lon with admin shapefiles
+coords_with_admin0 <- st_join(unique_coords_sf, africa_shp_admin0, join = st_within) %>%
+  rename(admin0_iso3c = iso) %>%
+  select(entry_iso3c, study_ID, admin0_iso3c)
+  # Retain entry_iso3c and study_ID
+coords_with_admin1 <- st_join(unique_coords_sf, africa_shp_admin1, join = st_within) %>%
+  mutate(
+    longitude = st_coordinates(.)[, 1],  # Extract longitude
+    latitude = st_coordinates(.)[, 2]   # Extract latitude
+  ) %>%
+  select(longitude, latitude, site_name, name_1) %>%
+  rename(admin1_name = name_1)
+
+# Combine admin0 and admin1
+coords_with_admin <- coords_with_admin1 %>%
+  st_join(coords_with_admin0)
+
+# Identify any general mismatches mismatches (currently 2 - 1 GEOFF, 1 WWARN), remove any non-African STAVE data (shape) - 628 studies
+mismatch_rows <- coords_with_admin %>%
+  filter(entry_iso3c != admin0_iso3c | !(entry_iso3c %in% admin0_iso3c)) %>%  # Compare entry_iso3c with admin0_iso3c
+  select(longitude, latitude, site_name, admin1_name, entry_iso3c, admin0_iso3c, study_ID)
+
+# Drop studies that are in mismatches
+mismatch_studies <- unique(mismatch_rows$study_ID)
+stave_clean = stave$clone()
+stave_clean$drop_study(mismatch_studies)
+# Confirm studies are dropped
+
+stave$print()
+stave_clean$print()
+
+### Pull Validated and Candidate Prevalences ###
+survey_clean <- stave_clean$get_surveys()
+counts <- stave_clean$get_counts()
+studies_clean <- stave_clean$get_studies()
+
+validated_mutations <- c("k13:446:I", "k13:458:Y", "k13:469:Y", "k13:476:I",   "k13:493:H",   "k13:539:T",
+                         "k13:543:T",  "k13:553:L",   "k13:561:H",   "k13:574:L",  "k13:580:Y",  "k13:622:I","k13:675:V")
+candidate_mutations <- c("k13:441:L", "k13:449:A",   "k13:469:F",   "k13:481:V",
+                         "k13:515:K", "k13:527:H",  "k13:537:I", "k13:537:D", "k13:538:V",  "k13:568:G")
+all_who_mutations <- c("k13:446:I", "k13:458:Y", "k13:469:Y", "k13:476:I",   "k13:493:H",   "k13:539:T",
+                       "k13:543:T",  "k13:553:L",   "k13:561:H",   "k13:574:L",  "k13:580:Y",  "k13:622:I","k13:675:V",
+                       "k13:441:L", "k13:449:A",   "k13:469:F",   "k13:481:V",
+                       "k13:515:K", "k13:527:H",  "k13:537:I", "k13:537:D", "k13:538:V",  "k13:568:G")
+
+all_prev_data  <- data.frame()
+#Loop over STAVE object to pull k13 mutations
+# for (selected_mutation in all_who_mutations){
+#   #call to stave for selected mutation, drop anything empty, set year from collection day
+#   #TO DO: potentially filter by year depending on mutation? mdr1 data extends to 96
+#   prevalence_data <- stave_clean$get_prevalence(selected_mutation) %>% drop_na() %>%
+#     mutate(year = substring(collection_day,0,4))
+#   prevalence_data$mutation <- selected_mutation
+#   all_prev_data <- bind_rows(all_prev_data,prevalence_data)
+# }
+# write.csv(all_prev_data, "analysis/data/validated_get_prevalence.csv", row.names = FALSE)
+# write.csv(all_prev_data, "analysis/data/validated_and_candidate_get_prevalence.csv", row.names = FALSE)
+
+all_prev_data <- read.csv("analysis/data/validated_and_candidate_get_prevalence.csv",header = TRUE)
+
+## Manipulating all-prev-data into overall k13 map ##
+
+k13_avg_prevalence <- all_prev_data %>% group_by(latitude, longitude, study_name, country_name, site_name,collection_day, year,denominator) %>%
+  summarize(k13_prevalence = sum(prevalence, na.rm = TRUE)) %>%
+  ungroup()
+
+africa_map_points <- ggplot() +
+  facet_wrap(~year) +
+  geom_sf(data = africa_shp_admin0, fill = NA, color = "black", show.legend = FALSE, lwd = 0.1) +
+
+  # Plot zero-prevalence points in grey
+  geom_point(data = filter(k13_avg_prevalence, k13_prevalence == 0),
+             aes(x = longitude, y = latitude, size = denominator),
+             color = "grey70", alpha = 0.8) +
+
+  # Plot non-zero prevalence points with color scale
+  geom_point(data = filter(k13_avg_prevalence, k13_prevalence > 0),
+             aes(x = longitude, y = latitude, color = k13_prevalence, size = denominator),
+             alpha = 0.8) +
+
+  scale_color_viridis_c(name = "Prevalence") +
+  scale_size_continuous(name = "Sample Size (N)", range = c(1, 10)) +
+  theme_void(base_size = 14) +
+  theme(
+    legend.position = "bottom",
+    plot.background = element_rect(fill = "white", color = "white")
+  )
+
+ggsave(
+  filename = paste0("analysis/plots/africa_map_k13_points_all_years_facet.png"),
+  plot = africa_map_points,
+  width = 12, height = 10, units = "in", dpi = 300
+)
+
+k13_avg_prevalence <- k13_avg_prevalence %>%
+  mutate (
+    year_group = case_when (
+      year %in% 2010:2014 ~ "2010-2014",
+      year %in% 2015:2019 ~ "2015-2019",
+      year == 2020 ~ "2020",
+      year == 2021 ~ "2021",
+      year == 2022 ~ "2022",
+      year == 2023 ~ "2023",
+      TRUE ~ NA_character_  # exclude years outside these groups
+    )
+  ) %>%
+    filter(!is.na(year_group))  # drop early/incomplete years
+
+africa_map_points <- ggplot() +
+  facet_wrap(~year_group) +
+  geom_sf(data = africa_shp_admin0, fill = NA, color = "black", show.legend = FALSE, lwd = 0.1) +
+
+  # Plot zero-prevalence points in grey
+  geom_point(data = filter(k13_avg_prevalence, k13_prevalence == 0),
+             aes(x = longitude, y = latitude, size = denominator),
+             color = "grey70", alpha = 0.8) +
+
+  # Plot non-zero prevalence points with color scale
+  geom_point(data = filter(k13_avg_prevalence, k13_prevalence > 0),
+             aes(x = longitude, y = latitude, color = k13_prevalence, size = denominator),
+             alpha = 0.8) +
+
+  scale_color_viridis_c(name = "Prevalence") +
+  scale_size_continuous(name = "Sample Size (N)", range = c(1, 10)) +
+  theme_void(base_size = 14) +
+  theme(
+    legend.position = "bottom",
+    plot.background = element_rect(fill = "white", color = "white")
+  )
+
+ggsave(
+  filename = paste0("analysis/plots/africa_map_k13_points_grey_zeros_facet.png"),
+  plot = africa_map_points,
+  width = 12, height = 10, units = "in", dpi = 300
+)
