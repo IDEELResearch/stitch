dc242d8d54744c99188c5d4d3668762a6a92b1ba
nnwyoung <neeva_young@brown.edu>
2025-05-13 10:19:25 -0400

    windows sux

--- analysis/XX_map_of_resistance_data.R
+++ analysis/XX_map_of_resistance_data.R
@@ -10,62 +10,13 @@ africa_shp_admin0 <- readRDS(file = rds_file_admin0)
 africa_shp_admin1 <- readRDS(file = rds_file_admin1)
 
 #Pull in Stave Data from cleaned & selected prevalence script
+stave_clean <- readRDS("analysis/data/stave_intermed_clean.rds")
 all_prev_data <- read.csv("analysis/data/validated_and_candidate_get_prevalence.csv", header = TRUE)
-
-# Clean Stave Data
-library(countrycode)  # for coordinating STAVE country with iso3c code
-
-# Get surveys from STAVE & identify countries for each lat lon
-survey <- stave$get_surveys()
-# Collapse entries to unique coords
-unique_coords <- survey %>%
-  select(latitude, longitude, site_name, country_name, study_key) %>%
-  distinct() %>%
-  mutate(
-    lat = as.numeric(latitude),
-    lon = as.numeric(longitude)
-  ) %>%
-  drop_na(lat, lon) %>%
-  rename(study_ID = study_key) %>%
-  mutate(entry_iso3c = countrycode(country_name, origin = "country.name", destination = "iso3c"))
-unique_coords_sf <- st_as_sf(unique_coords, coords = c("lon", "lat"), crs = 4326)
-# Align lat lon with admin shapefiles
-coords_with_admin0 <- st_join(unique_coords_sf, africa_shp_admin0, join = st_within) %>%
-  select(entry_iso3c, study_ID)
-
-%>%  # Retain entry_iso3c and study_ID
-  rename(admin0_iso3c = iso)
-coords_with_admin1 <- st_join(unique_coords_sf, africa_shp_admin1, join = st_within) %>%
-  mutate(
-    longitude = st_coordinates(.)[, 1],  # Extract longitude
-    latitude = st_coordinates(.)[, 2]   # Extract latitude
-  ) %>%
-  select(longitude, latitude, site_name, name_1) %>%
-  rename(admin1_name = name_1)
-# Combine admin0 and admin1
-coords_with_admin <- coords_with_admin1 %>%
-  st_join(coords_with_admin0)
-# Identify any general mismatches mismatches, remove any non-African STAVE data (shape)
-mismatch_rows <- coords_with_admin %>%
-  filter(entry_iso3c != admin0_iso3c | !(entry_iso3c %in% admin0_iso3c)) %>%  # Compare entry_iso3c with admin0_iso3c
-  select(longitude, latitude, site_name, admin1_name, entry_iso3c, admin0_iso3c, study_ID)
-# Drop studies that are in mismatches
-mismatch_studies <- unique(mismatch_rows$study_ID)
-stave_clean = stave$clone()
-stave_clean$drop_study(mismatch_studies)
-# Confirm studies are dropped
-stave$print()
-stave_clean$print()
 
 ### START MAPPING ###
 survey_clean <- stave_clean$get_surveys()
 counts <- stave_clean$get_counts()
 studies_clean <- stave_clean$get_studies()
-
-
-
-
-
 
 # Calculate the centroids for each MULTIPOLYGON
 sf_use_s2(FALSE)
@@ -80,12 +31,12 @@ africa_admin1_longlat <- africa_shp_admin1 %>%
 # Add prevalence data to admin 1 shape file
 africa_admin1_longlat_prev <- africa_admin1_longlat %>%
   left_join(prevalence_data, by = "name_1") %>%
-  filter(!is.na(sample_size))
+  filter(!is.na(denominator))
 
 # Plot the Africa map coloring districts by sample_size
 africa_map_sample_fill <- ggplot() +
   facet_wrap(~year) +
-  geom_sf(data = africa_admin1_longlat_prev, aes(fill = sample_size), color = "darkgrey", lwd = 0.05) +
+  geom_sf(data = africa_admin1_longlat_prev, aes(fill = denominator), color = "darkgrey", lwd = 0.05) +
   geom_sf(data = africa_shp_admin0, fill = NA, color = "black", show.legend = FALSE, lwd = 0.05) +
   theme_void(base_size = 14) +
   labs(fill = "Sample Size (N)") +
@@ -93,7 +44,7 @@ africa_map_sample_fill <- ggplot() +
   theme(legend.position = "bottom",
         plot.background = element_rect(fill = "white", color="white"))
 
-ggsave(filename="analysis/plots/africa_map_sample_fill.png", africa_map_sample_fill)
+ggsave(filename="analysis/plots/africa_map_sample_fill25.png", africa_map_sample_fill)
 
 
 #Loop over calls to cleaned stave product to map validated_mutations by year -> Supplemental Figure
