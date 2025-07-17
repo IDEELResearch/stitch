c7813ba0d91345c03b823150565d95eb85b68a47
cecile-meier-scherling <cecile_meier-scherling@brown.edu>
2025-07-16 20:54:31 -0400

    updated stave file, minor documentation changes to YY_prep file

--- analysis/YY_prep_stave_object.R
+++ analysis/YY_prep_stave_object.R
@@ -1,3 +1,6 @@
+# author: NWY
+# description: This file creates prevalence data from a stave obj.
+
 library(sf)       # for shapefile handling
 library(ggplot2)  # for plotting
 library(dplyr)    # for data manipulation
@@ -5,11 +8,10 @@ library(tidyr)    # for mild clean of stave data
 library(usethis)  # req'd for devtools
 library(here)     # req'd for devtools
 library(devtools) # for github code check
+library(countrycode) # for coordinating STAVE country with iso3c code
 
-devtools::check()
-#devtools::check_win_release()
-#devtools::document()
-#devtools::load_all()
+# disables the use of the S2 geometry engine for spatial operations
+sf_use_s2(FALSE)
 
 # Read in Africa shape file generated in 01_generative_base_maps.R
 rds_file_admin0 = "analysis/data_derived/sf_admin0_africa.rds"
@@ -19,16 +21,12 @@ africa_shp_admin1 <- readRDS(file = rds_file_admin1)
 
 #Pull in Stave Data
 #stave = readRDS(url("https://github.com/IDEELResearch/scrub/tree/931d07255aadc1e41005ed45c8345d0ac966b272/analysis/data-out"))
-stave <- readRDS("/home/nwernsma/Documents/scrub/analysis/data-out/stave_data.rds")
-
-# Clean Stave Data
-library(countrycode)  # for coordinating STAVE country with iso3c code
+stave <- readRDS("analysis/data_derived/stave_final_data.rds")
 
 # Get surveys from STAVE & identify countries for each lat lon
 survey <- stave$get_surveys()
 
 # Collapse entries to unique coords
-sf_use_s2(FALSE)
 unique_coords <- survey %>%
   select(latitude, longitude, site_name, country_name, study_key) %>%
   distinct() %>%
@@ -45,7 +43,8 @@ unique_coords_sf <- st_as_sf(unique_coords, coords = c("lon", "lat"), crs = 4326
 coords_with_admin0 <- st_join(unique_coords_sf, africa_shp_admin0, join = st_within) %>%
   rename(admin0_iso3c = iso) %>%
   select(entry_iso3c, study_ID, admin0_iso3c)
-  # Retain entry_iso3c and study_ID
+
+# Retain entry_iso3c and study_ID
 coords_with_admin1 <- st_join(unique_coords_sf, africa_shp_admin1, join = st_within) %>%
   mutate(
     longitude = st_coordinates(.)[, 1],  # Extract longitude
@@ -58,22 +57,20 @@ coords_with_admin1 <- st_join(unique_coords_sf, africa_shp_admin1, join = st_wit
 coords_with_admin <- coords_with_admin1 %>%
   st_join(coords_with_admin0)
 
-# Identify any general mismatches mismatches (currently 2 - 1 GEOFF, 1 WWARN), remove any non-African STAVE data (shape) - 628 studies
-mismatch_rows <- coords_with_admin %>%
-  filter(entry_iso3c != admin0_iso3c | !(entry_iso3c %in% admin0_iso3c)) %>%  # Compare entry_iso3c with admin0_iso3c
-  select(longitude, latitude, site_name, admin1_name, entry_iso3c, admin0_iso3c, study_ID)
-
-# Drop studies that are in mismatches
-mismatch_studies <- unique(mismatch_rows$study_ID)
-stave_clean = stave$clone()
-stave_clean$drop_study(mismatch_studies)
-# Confirm studies are dropped
-stave$print()
-stave_clean$print()
-stave_clean$get_counts()
-
-raw_breakdown <- all_prev_data %>% group_by(country_name, mutation) %>% summarise(sum(denominator))
-write.csv(raw_breakdown,"analysis/data/raw_sample_count_breakdown.csv",row.names = FALSE)
+# To-do: delete lines 60-73
+# # Identify any general mismatches mismatches (currently 2 - 1 GEOFF, 1 WWARN), remove any non-African STAVE data (shape) - 628 studies
+# mismatch_rows <- coords_with_admin %>%
+#   filter(entry_iso3c != admin0_iso3c | !(entry_iso3c %in% admin0_iso3c)) %>%  # Compare entry_iso3c with admin0_iso3c
+#   select(longitude, latitude, site_name, admin1_name, entry_iso3c, admin0_iso3c, study_ID)
+#
+# # Drop studies that are in mismatches
+# mismatch_studies <- unique(mismatch_rows$study_ID)
+# stave_clean = stave$clone()
+# stave_clean$drop_study(mismatch_studies)
+# # Confirm studies are dropped
+# stave$print()
+# stave_clean$print()
+# stave_clean$get_counts()
 
 ### Pull Validated and Candidate Prevalences ###
 survey_clean <- stave_clean$get_surveys()
@@ -93,14 +90,27 @@ all_who_mutations <- c("k13:446:I", "k13:458:Y", "k13:469:Y", "k13:476:I",   "k1
 
 all_prev_data  <- data.frame()
 #Loop over STAVE object to pull k13 mutations
-# for (selected_mutation in all_who_mutations){
-#   #call to stave for selected mutation, drop anything empty, set year from collection day
-#   #TO DO: potentially filter by year depending on mutation? mdr1 data extends to 96
-#   prevalence_data <- stave_clean$get_prevalence(selected_mutation) %>% drop_na() %>%
-#     mutate(year = substring(collection_day,0,4))
-#   prevalence_data$mutation <- selected_mutation
-#   all_prev_data <- bind_rows(all_prev_data,prevalence_data)
-# }
+start_time <- Sys.time()
+
+for (i in seq_along(validated_mutations)) {
+  selected_mutation <- validated_mutations[i]
+  print(paste0("Processing: ", selected_mutation))
+
+  prevalence_data <- stave_clean$get_prevalence(selected_mutation) %>%
+    drop_na() %>%
+    mutate(year = substring(collection_day, 0, 4))
+  prevalence_data$mutation <- selected_mutation
+  all_prev_data <- bind_rows(all_prev_data, prevalence_data)
+
+  # Mid-loop time logging
+  current_time <- Sys.time()
+  elapsed <- round(difftime(current_time, start_time, units = "secs"), 2)
+  print(paste("Elapsed time:", elapsed, "seconds (", i, "of", length(validated_mutations), ")"))
+}
+
+end_time <- Sys.time()
+print(paste("Total time taken:", round(difftime(end_time, start_time, units = "secs"), 2), "seconds"))
+
 # write.csv(all_prev_data, "analysis/data/validated_get_prevalence.csv", row.names = FALSE)
 # write.csv(all_prev_data, "analysis/data/validated_and_candidate_get_prevalence.csv", row.names = FALSE)
 
